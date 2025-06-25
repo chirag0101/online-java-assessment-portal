763efdd49444516efeede05351c6b5250f8ad808
Chirag <chirag.suryawanshi@irisbusiness.com>
2025-06-25 14:29:24 +0530

    indented

--- OnlineCompilerBackend/src/main/java/com/iris/OnlineCompilerBackend/controllers/AdminController.java
+++ OnlineCompilerBackend/src/main/java/com/iris/OnlineCompilerBackend/controllers/AdminController.java
@@ -6,15 +6,12 @@ import com.iris.OnlineCompilerBackend.dtos.ResetAdminPasswordReqDTO;
 import com.iris.OnlineCompilerBackend.models.ApiResponse;
 import com.iris.OnlineCompilerBackend.services.AdminService;
 import jakarta.validation.Valid;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("AdminService")
 public class AdminController {
-    private static final Logger log = LoggerFactory.getLogger(AdminController.class);
     @Autowired
     private AdminService adminService;
 
