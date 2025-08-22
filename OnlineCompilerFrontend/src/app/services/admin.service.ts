f70a8d6bb066755998ee48dd8ac58119d2be85ff
chirag.suryawanshi <chiragsuryavanshi01@gmail.com>
2025-08-22 14:54:58 +0530

    indented

--- OnlineCompilerFrontend/src/app/services/admin.service.ts
+++ OnlineCompilerFrontend/src/app/services/admin.service.ts
@@ -81,6 +81,7 @@ export class AdminService {
             sessionStorage.setItem('isAuthenticatedUser', 'false');
           }
           console.error('Authentication API error:', error);
+          alert("Login Failed!");
           throw error;
         })
       );
